7912a2d9969431778a02860d7aa0df987e36db76
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-10 08:30:44 +0100

    Java/Maven - create the empty console application

---
+++ java-maven/src/main/java/cz/petrfaltus/bank_account_val_conv_restclient/Program.java
@@ -0,0 +1,11 @@
+package cz.petrfaltus.bank_account_val_conv_restclient;
+
+import static java.lang.System.out;
+
+public class Program {
+
+    public static void main(String[] args) {
+        out.println("YES");
+    }
+
+}
