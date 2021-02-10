5882097cb784ecc407a7babbc0f9b54304d8243d
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-10 08:39:53 +0100

    Java/Maven - implement the web POST request

---
+++ java-maven/src/main/java/cz/petrfaltus/bank_account_val_conv_restclient/Web.java
@@ -0,0 +1,54 @@
+package cz.petrfaltus.bank_account_val_conv_restclient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Web {
+    public static final String URL_ADDRESS = "http://api.petrfaltus.net/bank_account/json/1.0";
+    public static final String CHARSET = "UTF-8";
+    public static final String USER_AGENT = "Petr Faltus Java Bank account validator and converter REST client";
+
+    public static String request(String requestBody) {
+        String output = Const.EMPTY_STRING; // the default output value
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(URL_ADDRESS);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            connection.addRequestProperty("User-Agent", USER_AGENT);
+            connection.setRequestMethod("POST");
+
+            OutputStream os = connection.getOutputStream();
+            byte[] requestBodyBytes = requestBody.getBytes(CHARSET);
+            os.write(requestBodyBytes);
+
+            InputStream is = connection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, CHARSET);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line ;
+                output += System.lineSeparator();
+            }
+
+            br.close();
+        } catch (Exception e) {
+            output = null; // indicates the web request failed
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return output;
+    }
+}
