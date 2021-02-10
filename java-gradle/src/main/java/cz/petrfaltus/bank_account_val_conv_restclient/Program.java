1fcb1fc29ad6023f54175b4006df30ff111b1e4f
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-10 12:02:39 +0100

    Java/Gradle - implement the web POST request

--- java-gradle/src/main/java/cz/petrfaltus/bank_account_val_conv_restclient/Program.java
+++ java-gradle/src/main/java/cz/petrfaltus/bank_account_val_conv_restclient/Program.java
@@ -3,9 +3,16 @@ package cz.petrfaltus.bank_account_val_conv_restclient;
 import static java.lang.System.out;
 
 public class Program {
+    public static final String MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service";
 
     public static void main(String[] args) {
-        out.println("YES");
+        String requestJsonCountries = "{ \"method_number\":1 }";
+        String replyJsonCountries = Web.request(requestJsonCountries);
+        if (replyJsonCountries == null) {
+            out.println(" - " + MESSAGE_ERROR_CONTACTING_SERVICE);
+            return;
+        }
+        out.println(replyJsonCountries);
     }
 
 }
