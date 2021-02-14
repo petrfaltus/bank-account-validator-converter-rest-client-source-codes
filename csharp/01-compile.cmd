d6240db54376296d8d7aa76e139caa06eca7ac3b
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-14 10:46:45 +0100

    C# - implement the JSON encoding and decoding for the REST request

--- csharp/01-compile.cmd
+++ csharp/01-compile.cmd
@@ -8,5 +8,7 @@ set EXECUTABLE=%OUT%\%PROJECT%.exe
 set ICON=ico\BankAccount.ico
 set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
 
+set DLL=%OUT%\Newtonsoft.Json.dll
+
 mkdir %OUT%
-"%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% %SOURCE%.cs Web.cs
+"%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% /reference:%DLL% %SOURCE%.cs Web.cs RestRequest1.cs RestReply1.cs
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
 
 namespace BankAccountValConvRestClient
 {
     public class Program
     {
         private static readonly string MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service";
+        private static readonly string MESSAGE_RECEIVED_ERROR = "received error";
 
         public static void Main(string[] args)
         {
-            string restRequestJsonCountries = "{ \"method_number\":1 }";
+            // all supported countries query
+            Console.WriteLine("All supported countries:");
+
+            RestRequest1 restRequest1 = new RestRequest1();
+
+            string restRequestJsonCountries = JsonConvert.SerializeObject(restRequest1);
 
             string restReplyJsonCountries;
             try
@@ -21,7 +30,18 @@ namespace BankAccountValConvRestClient
                 return;
             }
 
-            Console.WriteLine(restReplyJsonCountries);
+            RestReply1 restReply1 = JsonConvert.DeserializeObject<RestReply1>(restReplyJsonCountries);
+
+            if (restReply1.error_code != 0)
+            {
+                Console.WriteLine(" - " + MESSAGE_RECEIVED_ERROR + ": " + restReply1.error_string);
+                return;
+            }
+
+            foreach(KeyValuePair<string, string> country in restReply1.data)
+            {
+                Console.WriteLine(" - " + country.Key + " ... " + country.Value);
+            }
         }
     }
 }
