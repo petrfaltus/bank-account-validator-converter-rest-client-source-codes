81a4eb45373d64c6b055f5f1d2886c743ffc3c0b
Petr Faltus <petrfaltus@petrfaltus.net>
2021-02-15 09:38:09 +0100

    PHP - implement the web POST request

--- php/bank-account-val-conv-restclient.php
+++ php/bank-account-val-conv-restclient.php
@@ -1,5 +1,29 @@
 <?php
 
-echo "YES".PHP_EOL;
+// constants
+const PATH_TO_CLASSES = ".class";
+
+// user classes autoloading
+spl_autoload_register(function($class_name)
+{
+  $class_source_file_path = PATH_TO_CLASSES."/".$class_name.".php";
+  require_once $class_source_file_path;
+});
+
+const MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service";
+
+// all supported countries query
+echo "All supported countries:".PHP_EOL;
+
+$requestJsonCountries = "{ \"method_number\":1 }";
+
+$replyJsonCountries = tWeb::request($requestJsonCountries);
+if ($replyJsonCountries === null)
+{
+  echo " - ".MESSAGE_ERROR_CONTACTING_SERVICE.PHP_EOL;
+  return;
+}
+
+echo $replyJsonCountries.PHP_EOL;
 
 ?>
