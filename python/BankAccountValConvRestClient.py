388bbc08323ada552f999bcfed8812781188df17
Petr Faltus <petrfaltus@petrfaltus.net>
2021-03-30 11:51:47 +0200

    Python - implement the web POST request

--- python/BankAccountValConvRestClient.py
+++ python/BankAccountValConvRestClient.py
@@ -1,2 +1,16 @@
+from classes.Web import Web
 
-print("YES")
+MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service"
+
+# all supported countries query
+print("All supported countries:")
+
+requestJsonCountries = "{ \"method_number\": 1 }"
+
+try:
+    replyJsonCountries = Web.request(requestJsonCountries)
+except:
+    print(" - " + MESSAGE_ERROR_CONTACTING_SERVICE)
+    exit()
+
+print(replyJsonCountries)
