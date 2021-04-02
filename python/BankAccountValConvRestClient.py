8dfde124f93ef4b090ba85b2f865d4c7648072a1
Petr Faltus <petrfaltus@petrfaltus.net>
2021-04-02 13:14:50 +0200

    Python - implement the JSON encoding and decoding for the REST request

--- python/BankAccountValConvRestClient.py
+++ python/BankAccountValConvRestClient.py
@@ -1,11 +1,14 @@
 from classes.Web import Web
+from classes.Json import Json
 
 MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service"
+MESSAGE_ERROR_DECODING_JSON = "error decoding the reply JSON"
+MESSAGE_RECEIVED_ERROR = "received error"
 
 # all supported countries query
 print("All supported countries:")
 
-requestJsonCountries = "{ \"method_number\": 1 }"
+requestJsonCountries = Json.codeQueryCountries()
 
 try:
     replyJsonCountries = Web.request(requestJsonCountries)
@@ -13,4 +16,16 @@ except:
     print(" - " + MESSAGE_ERROR_CONTACTING_SERVICE)
     exit()
 
-print(replyJsonCountries)
+countries = Json.decodeResult(replyJsonCountries)
+if countries == None:
+    errorString = Json.getLastErrorString()
+
+    if errorString != None:
+        print(" - " + MESSAGE_RECEIVED_ERROR + ": " + errorString)
+    else:
+        print(" - " + MESSAGE_ERROR_DECODING_JSON)
+
+    exit()
+
+for countryCode in countries:
+    print(" - " + countryCode + " (" + countries[countryCode] + "}")
