f3b6fddfa1445b5327172c2692124cdee5357ac5
Petr Faltus <petrfaltus@petrfaltus.net>
2021-04-03 10:24:12 +0200

    Python - implement the JSON encoding and decoding for IBAN to local numbering request

--- python/BankAccountValConvRestClient.py
+++ python/BankAccountValConvRestClient.py
@@ -29,3 +29,35 @@ if countries == None:
 
 for countryCode in countries:
     print(" - " + countryCode + " (" + countries[countryCode] + "}")
+
+print()
+
+# IBAN validation and to local numbering conversion query
+print("IBAN validation and to local numbering conversion:")
+
+iban = "CZ6508000000192000145399"
+country = "cz" # returned in the all supported countries request
+
+requestJsonIbanToLocalNumbering = Json.codeQueryIbanToLocalNumbering(iban, country)
+
+try:
+    replyJsonIbanToLocalNumbering = Web.request(requestJsonIbanToLocalNumbering)
+except:
+    print(" - " + MESSAGE_ERROR_CONTACTING_SERVICE)
+    exit()
+
+replyLocalNumbering = Json.decodeResult(replyJsonIbanToLocalNumbering)
+if replyLocalNumbering == None:
+    errorString = Json.getLastErrorString()
+
+    if errorString != None:
+        print(" - " + MESSAGE_RECEIVED_ERROR + ": " + errorString)
+    else:
+        print(" - " + MESSAGE_ERROR_DECODING_JSON)
+
+    exit()
+
+print(" - IBAN human: " + replyLocalNumbering[Json.IBAN_HUMAN])
+print(" - account number identificator: " + replyLocalNumbering[Json.ACCOUNT_NUMBER_IDENTIFICATOR])
+print(" - bank code: " + replyLocalNumbering[Json.BANK_CODE])
+print(" - account number: " + replyLocalNumbering[Json.ACCOUNT_NUMBER])
--- python/classes/Json.py
+++ python/classes/Json.py
@@ -3,17 +3,35 @@ import json
 class Json:
     METHOD_NUMBER = "method_number"
 
+    IBAN = "iban"
+    COUNTRY = "country"
+
     ERROR_CODE = "error_code"
     ERROR_STRING = "error_string"
     DATA = "data"
 
+    IBAN_HUMAN = "iban_human"
+    ACCOUNT_NUMBER_IDENTIFICATOR = "account_number_identificator"
+    BANK_CODE = "bank_code"
+    ACCOUNT_NUMBER = "account_number"
+
     METHOD_COUNTRIES_NUMBER = 1
+    METHOD_IBAN_TO_LOCAL_NUMBERING_NUMBER = 2
 
     lastErrorString = None
 
     @staticmethod
     def codeQueryCountries():
         output = {Json.METHOD_NUMBER: Json.METHOD_COUNTRIES_NUMBER}
+        outputJson = json.dumps(output)
+
+        return outputJson
+
+    @staticmethod
+    def codeQueryIbanToLocalNumbering(iban, country):
+        output = {Json.METHOD_NUMBER: Json.METHOD_IBAN_TO_LOCAL_NUMBERING_NUMBER,
+                  Json.IBAN: iban,
+                  Json.COUNTRY: country}
         outputJson = json.dumps(output)
 
         return outputJson
